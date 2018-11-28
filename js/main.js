1894054eb2e5a9937869729ee0e30be69b261ccd
y.zharsky <y.zharsky@zebrains.team>
2018-11-28 12:38:14 +0300

    desktop

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
     $(this).toggleClass('is-active');
     $('nav').slideToggle('300');
   });
+
+  var pillowsSwiper = new Swiper('.swiper-container-pillows', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    loop: true
+  });
 });
