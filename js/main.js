ca25bfc2d1aec3e5286502befdabd3f88d175ef2
y.zharsky <y.zharsky@zebrains.team>
2018-11-28 14:00:02 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('.hamburger').click(function(){
     $(this).toggleClass('is-active');
     $('nav').slideToggle('300');
+    $('.logotype').fadeToggle('300');
   });
 
   var pillowsSwiper = new Swiper('.swiper-container-pillows', {
@@ -9,7 +10,16 @@ $(document).ready(function() {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
     slidesPerView: 3,
-    loop: true
+    loop: true,
+    breakpoints: {
+      768:{
+        slidesPerView: 1
+      }
+    }
   });
 });
